063b38c32735d822e53d7bc6ffb99f9a01046154
ndurbin <nick.durbin@gmail.com>
2020-02-29 01:54:00 -0500

    Examples of basic ypes in JS and TS.

--- app.ts
+++ app.ts
@@ -1,19 +1,15 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-  //   throw new Error('Incorrect input!');
-  // }
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return result;
-  }
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+  userName = userInput;
 }
 
-let number1: number;
-number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-let resultPhrase = 'Result is: ';
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+  // while (true) {}
+}
 
-add(number1, number2, printResult, resultPhrase);
+generateError('An error occurred!', 500);
